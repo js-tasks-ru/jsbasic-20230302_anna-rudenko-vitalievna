61d93133603bf5edd392ee789be4cf584e33ed6d
DemonicDragons <annrudenko9@gmail.com>
2023-03-05 01:22:36 +0900

    Модуль 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+    for(let key in salaries){
+      if (isFinite(salaries[key]) && !isNaN(salaries[key]) && typeof salaries[key] !== 'boolean'){
+        sum += salaries[key];
+      }else if (salaries[key] == 'number'){
+        sum = 0;
+      }
+    }
+  return sum;
 }
