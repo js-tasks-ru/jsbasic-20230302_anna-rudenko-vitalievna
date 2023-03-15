77055f814d8a607c5b521d4e401738723f16f291
DemonicDragons <126694624+DemonicDragons@users.noreply.github.com>
2023-03-15 16:36:16 +0900

    Update index.js

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -16,7 +16,7 @@ function print(text) {
  * @returns {boolean}
  */
 function isValid(name) {
-  return name == null || name=='' || name.length < 4 || name.includes(' ');
+  return (name == null || name=='' || name.length < 4 || name.includes(' '));
 }
 
 /**
