5f92a7f58001f859dd59883e4dd5c8ef93b58524
DemonicDragons <126694624+DemonicDragons@users.noreply.github.com>
2023-03-17 01:33:48 +0900

    Update index.js

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,9 +1,11 @@
 function getMinMax(str) {
-  const Arr = str.split(' ');
-  const min = Math.min.apply(null, Arr);
-  const max = Math.max.apply(null, Arr);
-  return Obj = {
-    min,
-    max,
-  };
+  // ваш код...
+  let numbersOnly = str
+  .split(',').join(' ').split(' ')
+  .filter(item => Number(item));
+  let result = {
+    min: Math.min(...numbersOnly),
+    max: Math.max(...numbersOnly),
+  }
+  return result;
 }
