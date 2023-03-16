62e01dd4749b727b05b3eab59edd03e2491e5655
DemonicDragons <126694624+DemonicDragons@users.noreply.github.com>
2023-03-17 01:11:01 +0900

    Update index.js

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,10 +1,4 @@
 function isEmpty(obj) {
-  for(let key in obj){
-    if (obj.hasOwnProperty(key)){
-      return true;
-    }
-  }
-  return false;
     for(let key in obj){
         return false;
     }
