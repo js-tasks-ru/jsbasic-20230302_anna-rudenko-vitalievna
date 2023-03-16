cfb410d0082846ba96cbdf131f5a7603f2d3f50c
DemonicDragons <126694624+DemonicDragons@users.noreply.github.com>
2023-03-17 01:31:38 +0900

    Update index.js

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,4 +1,4 @@
 function showSalary(users, age) {
-  let NewArr = users.filter((i,age) => i.age <= ages);
+  let NewArr = users.filter(i => (i.age <= age));
   return NewArr.map(i => `${i.name}, ${i.age}`).join('\n');
 }
